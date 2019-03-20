8d17357e69d19d28317cd25aacc9496bd7e7311e
jtimothySHU <47188098+jtimothySHU@users.noreply.github.com>
2019-03-20 10:42:06 -0400

    addingCameraFollowing

---
+++ Assets/Scripts/followBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followBall : MonoBehaviour
+{
+
+    public GameObject ball;
+    public Transform player;
+
+    public Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            transform.position = player.position + (new Vector3(0, 0, 4));
+        }
+    }
+}
