ca71c90255e0a5808e98d16ef2eaf84519e0882e
jtimothySHU <47188098+jtimothySHU@users.noreply.github.com>
2019-03-27 10:12:03 -0400

    Update throwBall.cs

--- Assets/Scripts/throwBall.cs
+++ Assets/Scripts/throwBall.cs
@@ -69,7 +69,7 @@ public class throwBall : MonoBehaviour
                 xForce = xDistance * 0.1f;
 
             yForce = yDistance * 0.1f;
-            zForce = yDistance * -0.1f;
+            zForce = yDistance * -2;
 
             rigidBall.AddForce(xForce, yForce, zForce);
 
