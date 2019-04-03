158af31b31eace6df660d915eb1c8ed9578e9945
jtimothySHU <47188098+jtimothySHU@users.noreply.github.com>
2019-04-02 20:48:52 -0400

    ScoreUpdates

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This is necessary for UI elements
+using UnityEngine.UI;
+
 public class score : MonoBehaviour
 {
 
     public Transform player;
+    public Text scoreText;
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(player.position.z);
+        scoreText.text = player.position.z.ToString();
     }
 }
