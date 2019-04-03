202fbb4a799b6220dfe3daf87d9f5b49a15c5de7
jtimothySHU <47188098+jtimothySHU@users.noreply.github.com>
2019-04-02 20:43:51 -0400

    ScoreUIv1

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.position.z);
+    }
+}
