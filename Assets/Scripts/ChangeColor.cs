9d6ac4229709681821a434754a49ccde421f7663
jtimothySHU <47188098+jtimothySHU@users.noreply.github.com>
2019-04-09 13:04:46 -0400

    settingStageForColorChange

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public static Material ballColor;
+
+    // Update is called once per frame
+    public void ChangeTheColor(Material thisColor)
+    {
+        ballColor = thisColor;
+    }
+}
