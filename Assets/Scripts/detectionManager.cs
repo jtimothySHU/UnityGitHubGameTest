d60decc0e4ea80aeb1be89c7be98890ab68f42f4
Owen Keith <granut100@gmail.com>
2019-04-20 13:35:26 -0400

    Fixes
    *Ball will reset after hiting the target
    *Score counter also works for the targets. I don't know about the blue targets/ I'll add those later or something.

---
+++ Assets/Scripts/detectionManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectionManager : MonoBehaviour
+{
+    //Score for all the targets
+    public int score;
+   
+}
