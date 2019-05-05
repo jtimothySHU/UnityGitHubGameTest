6766b38bab82bd0f64a45499a40ff1bfdeaf8422
jtimothySHU <47188098+jtimothySHU@users.noreply.github.com>
2019-05-05 19:28:05 -0400

    OptionsMenuChanges

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeColor : MonoBehaviour
 {
     static public int colorChoice;
+    public Text colorSelectionTextTitle;
+    public string colorText;
 
     // on click
     public void ChangeTheColor(int thisColor)
     {
         colorChoice = thisColor;   
     }
+
+    public void ChangeTheText(string myColor)
+    {
+        colorText = myColor;
+        colorSelectionTextTitle.text = colorText;
+
+    }
+
+
 }
